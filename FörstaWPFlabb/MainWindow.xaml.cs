6903e9867d5bc2583cef767f66f2c3d1bc5f3ede
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-08-26 10:30:12 +0200

    Tränat på ytlig wpf kodning

---
+++ FörstaWPFlabb/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FörstaWPFlabb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Skriv text i TextBlock
+            textruta.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        }
+
+        private void Skicka_Click(object sender, RoutedEventArgs e)
+        {
+            // Skriv ut en dialogruta
+            //MessageBox.Show(inputruta.Text);
+
+            string inTexten = "";
+
+            // Läs in från formuläret
+            inTexten = inputruta.Text;
+
+            // Skriv ut i formuläret
+            textruta.Text = inTexten;
+        }
+
+    }
+}
---
+++ FörstaWPFlabb2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FörstaWPFlabb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Skicka_Click(object sender, RoutedEventArgs e)
+        {
+            // Skriv ut en dialogruta
+            //MessageBox.Show(inputruta.Text);
+
+            string inTexten = "";
+
+            // Läs in från formuläret
+            inTexten = inputruta.Text;
+            string omvandText = "";
+            for (int i = inTexten.Length; i > 0; i--)
+            {
+                omvandText += inTexten[i - 1];
+            }
+
+            // Skriv ut i formuläret
+            textruta.Text = omvandText;
+        }
+
+    }
+}

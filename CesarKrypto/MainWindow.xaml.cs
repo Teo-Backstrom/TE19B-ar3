3b1b833124a725e752f978acdb5a8fc3b5701825
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-09-02 11:02:48 +0200

    arbetat med problemsökning i cesarkryptokod

---
+++ CesarKrypto/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CesarKrypto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Skicka_Click(object sender, RoutedEventArgs e)
+        {
+
+            string text = inmatningText.Text;
+
+            string nyckelString = inmatningNyckel.Text;
+
+            int nyckel = 0;
+            if (!int.TryParse(nyckelString, out nyckel))
+            {
+                textruta.Text += " \n Du måste mata in en int, Försök igen: ";
+            }
+
+            string kryptadText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                char bokstav = text[i];
+                //  textruta.Text+=" \n Bokstaven på position " + i + " är " + bokstav;
+
+                int ascii = (int)bokstav;
+                // textruta.Text+=  "\n" + bokstav + " är "+ ascii;
+
+                int kryptadAscii = ascii + nyckel;
+
+                if (kryptadAscii >= 90)
+                {
+                    kryptadAscii -= 26;
+                }
+
+                char kryptadBokstav = (char)(kryptadAscii);
+                //textruta.Text += " \n " + bokstav+ " blir " + kryptadBokstav;
+
+                kryptadText += kryptadBokstav.ToString();
+            }
+            if (int.TryParse(nyckelString, out nyckel))
+            {
+                textruta.Text += " \n Den krypterade texten blir: " + kryptadText;
+            }
+
+        }
+
+
+    }
+}
---
+++ CesarKryptoUtanWpf/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CesarKryptoUtanWpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange en text som skall krypteras: ");
+            string text = Console.ReadLine();
+
+            Console.Write("Ange en nyckel (1-9): ");
+            string nyckelString = Console.ReadLine();
+
+            int nyckel = 0;
+            while (!int.TryParse(nyckelString, out nyckel))
+            {
+                Console.WriteLine("Du måste mata in ett tal (1-9). Försök igen: ");
+                nyckelString = Console.ReadLine();
+            }
+
+            string kryptadText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                char bokstav = text[i];
+                Console.WriteLine($"Bokstaven på position {i} är {bokstav}");
+
+                int ascii = (int)bokstav;
+                Console.WriteLine($"{bokstav} är {ascii}");
+
+                int kryptadAscii = ascii + nyckel;
+
+                if (kryptadAscii >= 90)
+                {
+                    kryptadAscii -= 26;
+                }
+
+                char kryptadBokstav = (char)(kryptadAscii);
+                Console.WriteLine($"{bokstav} blir {kryptadBokstav}");
+
+                kryptadText += kryptadBokstav.ToString();
+            }
+
+            Console.WriteLine($"Den krypterade texten blir: {kryptadText}");
+        }
+    }
+}

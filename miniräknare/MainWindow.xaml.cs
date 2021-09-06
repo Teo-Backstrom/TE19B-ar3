5b59fbb1bc1233c00d3e09be8eddd8a05593c112
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-09-06 13:28:14 +0200

    miniräknare

---
+++ miniräknare/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.IO.Compression;
+using System.ComponentModel;
+using System.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace miniräknare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void plus(object sender, RoutedEventArgs e){
+        double x = double.Parse(tal1.Text);
+        double y = double.Parse(tal2.Text);
+
+        double z = x + y;
+        resultat.Text = z.ToString();
+}
+        private void minus(object sender, RoutedEventArgs e){
+        double x = double.Parse(tal1.Text);
+        double y = double.Parse(tal2.Text);
+
+        double z = x - y;
+        resultat.Text = z.ToString();
+}
+        private void multi(object sender, RoutedEventArgs e){
+        double x = double.Parse(tal1.Text);
+        double y = double.Parse(tal2.Text);
+
+        double z = x * y;
+        resultat.Text = z.ToString();
+}
+        private void dela(object sender, RoutedEventArgs e){
+        double x = double.Parse(tal1.Text);
+        double y = double.Parse(tal2.Text);
+
+        double z = x / y;
+        resultat.Text = z.ToString();
+}
+    }
+
+}

98358dccfcc2c8746a4a55e6e1395fc8e463bef2
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-09-13 13:11:31 +0200

    gjort en filläsare med en filöppnare

---
+++ Filöppnare med filöppnare/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace WpfTutorialSamples.Dialogs
+{
+	public partial class OpenFileDialogSample : Window
+	{
+		public OpenFileDialogSample()
+		{
+			InitializeComponent();
+		}
+
+		private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+		{
+
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+			if(openFileDialog.ShowDialog() == true)
+				txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+
+		}
+	}
+}

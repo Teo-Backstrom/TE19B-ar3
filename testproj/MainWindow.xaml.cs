bf7fe25af576be8b3b95084ace41f1b59a9bd929
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-11-18 13:17:59 +0100

    fungerande kod

---
+++ testproj/MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+
+namespace testproj
+{
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Skapar en tom lista
+        List<PersonPoints> listaPerson = new List<PersonPoints>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Om filen finns ta ned json till list
+            if (File.Exists("personpoints.json"))
+            {
+                string jsonString = File.ReadAllText("personpoints.json");
+
+                listaPerson = JsonConvert.DeserializeObject<List<PersonPoints>>(jsonString);
+                Resultat();
+            }
+
+
+        }
+
+        /// <summary>
+        /// För att ska en person
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddPerson(object sender, RoutedEventArgs e)
+        {
+            //flagga
+            bool flagga = true;
+            //Skapar nya objekt
+            PersonPoints person = new PersonPoints();
+            //Bestämmer namnet
+            person.Namn = namn.Text;
+            //kollar om det finns dublett
+            if (listaPerson != null)
+            {
+
+                foreach (var P in listaPerson)
+                {
+                    if (P.Namn.ToLower() == person.Namn.ToLower())
+                    {
+                        //Felmedelande och sätter flagga så den inte sparas
+                        feedback.Text = "Person finns redan";
+                        flagga = false;
+                        break;
+                    }
+                    //Annars sparar personen
+                    flagga = true;
+                }
+            }
+            if (flagga)
+            {
+                listaPerson.Add(person);
+                SparJson();
+                //Visa resultat
+                Resultat();
+            }
+
+        }
+
+
+        /// <summary>
+        /// Skapar poäng till en person
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddSocialPoints(object sender, RoutedEventArgs e)
+        {
+            foreach (var person in listaPerson)
+            {
+                if (person.Namn.ToLower() == namnPoints.Text.ToLower())
+                {
+                    //Adderar poäng
+                    person.SocialPoints += ReadInt(points.Text);
+
+                    //Sparar och visar resultat
+                    SparJson();
+                    Resultat();
+                    break;
+                }
+            }
+
+        }
+        /// <summary>
+        /// Tar bort poäng frn en person
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SubtractSocialPoints(object sender, RoutedEventArgs e)
+        {
+            foreach (var person in listaPerson)
+            {
+                if (person.Namn.ToLower() == namnPoints.Text.ToLower())
+                {
+                    //Subtraherrar poäng
+                    person.SocialPoints -= ReadInt(points.Text);
+
+                    //Sparar och visar resultat
+
+                    SparJson();
+                    Resultat();
+                    break;
+                }
+            }
+
+        }
+        /// <summary>
+        /// Skapar poäng till en person
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddWorkingPoints(object sender, RoutedEventArgs e)
+        {
+            foreach (var person in listaPerson)
+            {
+                if (person.Namn.ToLower() == namnPoints.Text.ToLower())
+                {
+                    //Adderar poäng
+
+                    person.WorkingPower += ReadInt(points.Text);
+
+                    //Sparar och visar resultat
+                    SparJson();
+                    Resultat();
+                    break;
+                }
+            }
+
+        }
+        /// <summary>
+        ///  Tar bort poäng frn en person
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SubtractWorkingPoints(object sender, RoutedEventArgs e)
+        {
+            //Hittar personen genom att loopa
+            foreach (var person in listaPerson)
+            {
+                if (person.Namn.ToLower() == namnPoints.Text.ToLower())
+                {
+                    //tar bort poäng
+                    person.WorkingPower -= ReadInt(points.Text);
+
+                    //Sparar och visar resultat
+                    SparJson();
+                    Resultat();
+                    break;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Sparar json från list
+        /// </summary>
+        private void SparJson()
+        {
+            // Spara ned som json med formatting
+            string JsonText = JsonConvert.SerializeObject(listaPerson, Formatting.Indented);
+            File.WriteAllText("personpoints.json", JsonText);
+        }
+
+        /// <summary>
+        /// Visar resultatet i rutan längt ned
+        /// </summary>
+
+        private void Resultat()
+        {
+            //Tömmer textrutan
+            resultat.Text = "";
+            //Om listan är null så körs inte loopen
+            if (listaPerson != null)
+            {
+                foreach (var person in listaPerson)
+                {
+                    resultat.Text += "Namn: " + person.Namn + "\n" + "Social-Points: " + person.SocialPoints + "\n" + "Working-Power: " + person.WorkingPower + "\n \n";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Metoden för säkert sätt att läsa heltal
+        /// </summary>
+        /// <returns>Heltalet</returns>
+        int ReadInt(string tal)
+        {
+            int heltal;
+            if (!int.TryParse(tal, out heltal))
+            {
+                feedback.Text = "Du skrev inte in ett heltal. Försök igen.";
+            }
+            return heltal;
+        }
+    }
+}
---
+++ testproj/PersonPoints.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace testproj
+{
+    /// <summary>
+    /// Class med 3 objekt Namn SocialPoints och WorkingPower
+    /// </summary>
+    public class PersonPoints
+    {
+        public string Namn = "";
+        public int SocialPoints = 0;
+        public int WorkingPower = 0;
+    }
+}

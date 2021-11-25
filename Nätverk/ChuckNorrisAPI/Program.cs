a2c9a1710f608774c628cbe4d3f0acdc4d7d03fa
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-11-25 11:09:02 +0100

    tränat med api

---
+++ Nätverk/ChuckNorrisAPI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RestSharp;
+using System.Text.Json;
+
+namespace ChuckNorrisAPI
+{
+    class Joke
+    {
+        public string icon_url { get; set; }
+        public string value { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chuck Norris skämt");
+
+            // Hämta ett skämt från api.chucknorris.io
+            string url = "https://api.chucknorris.io/jokes";
+            var client = new RestClient(url);
+            var request = new RestRequest("random", DataFormat.Json);
+            var response = client.Get(request);
+
+            //KOLLA PÅ SVARET
+            //   Console.WriteLine(response.Content);
+
+            //Skapa ny objekt
+            Joke skämtet = new Joke();
+
+            //Avkoda json svaret
+            skämtet = JsonSerializer.Deserialize<Joke>(response.Content);
+
+            Console.WriteLine(skämtet.value);
+
+
+        }
+    }
+}
---
+++ Nätverk/PokeApi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using RestSharp;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace PokeApi
+{
+    class Pokemon
+    {
+        public string name { get; set; }
+    }
+
+    class Pokemons
+    {
+        public int count { get; set; }
+        public List<Pokemon> results { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hämta Pokemon info");
+
+            //hämta ett skämt
+            string url = "https://pokeapi.co/api/v2";
+            var client = new RestClient(url);
+            var request = new RestRequest("pokemon", DataFormat.Json);
+            var response = client.Get(request);
+
+            //kollar på svaret
+            //  Console.WriteLine(response.Content);
+            //Skapa ny objekt
+            Pokemons lista = new Pokemons();
+
+            //Avkoda json svaret
+            lista = JsonSerializer.Deserialize<Pokemons>(response.Content);
+
+            //skriv ut alla pokemons
+
+            if (lista != null)
+            {
+                foreach (var item in lista.results)
+                {
+                    Console.WriteLine(item.name);
+                }
+            }
+
+
+
+        }
+    }
+}
